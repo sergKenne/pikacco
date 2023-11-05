2edc7ab3947b53b9059c7cd37e03c0b481dbfebc
Serge Simplice Kenne <kenne_serge@mail.ru>
2023-11-05 11:54:47 +0300

    added store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-//import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import './index.scss';
 import App from './App';
-//import { apiPosts } from './store/features/apiPosts';
 import { Provider } from 'react-redux';
 import store from './store';
 
